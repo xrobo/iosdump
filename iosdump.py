ccd0c73c759a7ba2e0626325a71623ead2d86d0c
xrobo <timur@xrobo.ru>
2019-03-24 21:14:03 +0300

    new function get_hostname

--- iosdump.py
+++ iosdump.py
@@ -1,35 +1,47 @@
 #!/usr/bin/python
 
 import sys
+import os
+import re
 
-srorage="/opt/cisco/dumps"
-archive="/opt/cisco/archive"
+srorage = "/srv/iosdump/data/dumps"
+archive = "/srv/iosdump/data/archive"
+
 
 def page(code, body=None):
-  html = 'Status: ' + str(code) + '\n' + \
-         'Content-Type: text/plain' + '\n\n' + \
-         body
-  print html
-  sys.exit(0)
+    html = 'Status: ' + str(code) + '\n' + \
+           'Content-Type: text/plain' + '\n\n' + \
+           body
+    print html
+    sys.exit(0)
 
 
 def dump():
-  input = sys.stdin.read()
-  if len(input) > 100000:
-    page(500, "Content body too long")
-  page(200, input)
+    postdata = sys.stdin.read()
+    if len(postdata) > 100000:
+        page(500, "Content body too long")
+    hostname = get_hostname(postdata)
+    page(200, hostname)
+
+
+def get_hostname(postdata):
+    match = re.search('hostname (\w+)', postdata)
+    if match:
+        return match.group(1)
+    else:
+        return 'unknown'
 
 
 def main():
-  method = os.environ.get('REQUEST_METHOD')
-  if method:
-    if method == 'POST':
-      dump()
+    method = os.environ.get('REQUEST_METHOD')
+    if method:
+        if method == 'POST':
+            dump()
+        else:
+            page(405, 'This URI should be used with a POST method only')
     else:
-      page(405, 'This URI should be used with a POST method only')
-  else:
-    print 'This is a CGI-script that should be run using a web-client'
+        print 'This is a CGI-script that should be run using a web-client'
 
 
-if __name__=="__main__":
-  main()
+if __name__ == "__main__":
+    main()
