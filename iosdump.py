a1851803095c0d2f6c51e3d736261304cbf62f54
xrobo <timur@xrobo.ru>
2019-03-25 22:22:33 +0300

    first completion

--- iosdump.py
+++ iosdump.py
@@ -3,8 +3,9 @@
 import sys
 import os
 import re
+from datetime import datetime
 
-srorage = "/srv/iosdump/data/dumps"
+storage = "/srv/iosdump/data/dumps"
 archive = "/srv/iosdump/data/archive"
 
 
@@ -16,12 +17,27 @@ def page(code, body=None):
     sys.exit(0)
 
 
-def dump():
+def backup(hostname):
+    arcdir = archive + '/' + hostname
+    arcfile = arcdir + '/' + hostname + '_' + \
+        datetime.now().strftime("%Y.%m.%d.%H%M%S") + '.conf'
+    curfile = storage + '/' + hostname + '.conf'
+    if os.path.exists(curfile):
+        if not os.path.exists(arcdir):
+            os.mkdir(arcdir)
+        os.rename(curfile, arcfile)
+
+
+def catch():
     postdata = sys.stdin.read()
     if len(postdata) > 100000:
         page(500, "Content body too long")
     hostname = get_hostname(postdata)
-    page(200, hostname)
+    backup(hostname)
+    fh = open(storage + '/' + hostname + '.conf', "w")
+    fh.write(postdata)
+    fh.close()
+    page(201, 'saved')
 
 
 def get_hostname(postdata):
@@ -36,7 +52,7 @@ def main():
     method = os.environ.get('REQUEST_METHOD')
     if method:
         if method == 'POST':
-            dump()
+            catch()
         else:
             page(405, 'This URI should be used with a POST method only')
     else:
